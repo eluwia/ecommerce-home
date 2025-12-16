a8e418b7ca234a8a4116f204c14675dbf7133687
Elif Aydınlık <elifaydnlk@hotmail.com>
2025-12-16 20:34:21 +0300

    initial commit
    feat: add basic Button component

---
+++ components/ui/Button.vue
@@ -0,0 +1,84 @@
+<template>
+    <button
+    class="btn"
+    :class="[
+        'btn--'+ variant,
+        'btn--'+ size,
+        full ? 'btn--full' : '',
+    ]"
+    >
+        <slot />
+    </button>
+</template>
+
+<script setup>
+    defineProps({
+        variant: {
+            type: String,
+            default: 'primary',
+        },
+        size: {
+            type: String,
+            default: 'md',
+        },
+        full: {
+            type: Boolean,
+            default: false,
+        },
+    });
+</script>
+
+<style scoped>
+
+.btn {
+    border: none;
+    border-radius: 6px;
+    font-weight: 600;
+    cursor: pointer;
+}
+
+.btn--sm {
+    padding: 8px 12px;
+    font-size: 14px;
+}
+
+.btn--md {
+    padding: 12px 16px;
+    font-size: 16px;
+}
+
+.btn--lg {
+    padding: 16px 20px;
+    font-size: 18px;
+}
+
+.btn--primary {
+    background-color: #ff2f92;
+    color:white;
+}
+
+.btn--secondary {
+    background-color: #00a8a8;
+    color:white;
+}
+
+.btn--ghost {
+    background-color: transparent;
+    color: #00a8a8;
+}
+
+/* button hover for desktop view */
+
+@media (hover: hover) {
+    .btn--primary:hover {
+        background-color: #e02680;
+    }
+    .btn--secondary:hover {
+        background-color: #008f8f;
+    }
+    .btn--ghost:hover {
+        text-decoration: underline;
+    }
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="page">
+        <Button/>
+    </div>
+</template>
+
+<script setup>
+    import Button from '@/components/ui/Button.vue';
+</script>
+
+<style scoped>
+    .page {
+        padding: 20px;
+    }
+</style>
